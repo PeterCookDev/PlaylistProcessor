896b3a857a34f2fbc48a2f8da00457a0749751f3
Peter Cook <PeterCookdev@hotmail.com>
2017-04-05 21:05:27 +1000

    Added support for YT exported data

--- link_processing.py
+++ link_processing.py
@@ -16,8 +16,9 @@ def token_replacement(to_replace):
     tokens = [', LSC14', 'RailsConf 2016 - ', '33rd Degree 2014 - ', ' - PyCon 2016', 'Ruby On Ales 2016: ']
     tokens.extend(['DevOpsDays Seattle 2016 - ', 'GORUCO 2016 - ', 'Full Stack Fest 2015: '])
     tokens.extend([' - PyCon 2015', 'ArrrrCamp 2015 - ', 'RubyConf 2015 - ', 'GOTO 2015 ', 'e4e Developer Conf 2015 - '])
-    tokens.extend(['Ruby On Ales 2015 - ', 'Cascadia Ruby 2014- ', 'Rails Pacific 2016 - ', 'GOTO 2016 '])
-
+    tokens.extend(['Ruby On Ales 2015 - ', 'Cascadia Ruby 2014- ', 'Rails Pacific 2016 - ', 'GOTO 2016 ','Rocky Mountain Ruby 2016 -'])
+    tokens.extend(['PyDX 2015:','PyDX 2016: s','CodeDaze 2016 -','Keep Ruby Weird 2016 -','RubyConf 2016 -','YOW! CTO Summit 2016','DevTernity 2016:','YOW! 2016'])
+    
     for token in tokens:
         to_replace = to_replace.replace(token, "")
 
